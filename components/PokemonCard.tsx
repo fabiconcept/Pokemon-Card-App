341e7a164458955511c9c5b56c3ee0aa410e1e8e
fabiconcept <favourajokubi@gmail.com>
2024-12-28 14:19:14 +0100

    `Update README.md and components/PokemonCard.tsx with new content`

--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -1,9 +1,30 @@
-import { Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 
 export default function PokemonCard() {
     return (
-        <View>
+        <View style={styles.card}>
             <Text> Pokemon Card </Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: "white",
+        borderRadius: 16,
+        borderWidth: 2,
+        padding: 16,
+        margin: 16,
+        ...Platform.select({
+            ios: {
+                shadowColor: "#333",
+                shadowOffset: { width: 2, height: 2 },
+                shadowOpacity: 0.3,
+                shadowRadius: 4
+            },
+            android: {
+                elevation: 10
+            }
+        })
+    }
+});
