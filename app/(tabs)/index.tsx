e32011ef39fb8e6276d2caae867b760895aa5fb9
fabiconcept <favourajokubi@gmail.com>
2024-12-28 14:10:54 +0100

    `Add PokemonCard component and SafeAreaView to index.tsx`

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,18 @@
-import { View } from "react-native";
+import PokemonCard from "@/components/PokemonCard";
+import { StyleSheet, SafeAreaView, Platform } from "react-native";
 
 export default function index() {
   return (
-    <View>index</View>
+    <SafeAreaView style={styles.container}>
+      <PokemonCard />
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    paddingTop: Platform.OS === "android" ? 25 : 0,
+  },
+})
